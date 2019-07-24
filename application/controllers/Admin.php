683cc35f07e687eab33e2c118606b5defcd83fc8
AlfaRiza <malfariza45@gmail.com>
2019-07-24 23:22:20 +0700

    memperbaiki login admin/user dari data base

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -1,16 +1,16 @@
 <?php
 
-class User extends CI_Controller
+class Admin extends CI_Controller
 {
     public function index()
     {
-        $data['judul'] = 'Dashboard';
+        $data['judul'] = 'My Profile';
         $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
         // echo 'Selamat datang ' . $data['user']['name'];
         $this->load->view('templates/header', $data);
         $this->load->view('templates/sidebar', $data);
         $this->load->view('templates/topbar', $data);
-        $this->load->view('user/index', $data);
+        $this->load->view('admin/index', $data);
         $this->load->view('templates/footer');
     }
 }
---
+++ application/views/templates/sidebar.php
@@ -0,0 +1,68 @@
+        <!-- Sidebar -->
+        <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+            <!-- Sidebar - Brand -->
+            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+                <div class="sidebar-brand-icon">
+                    <i class="fas fa-user-secret"></i>
+                </div>
+                <div class="sidebar-brand-text mx-3">Admin<sup><i class="fab fa-affiliatetheme"></i></sup></div>
+            </a>
+
+            <!-- Divider -->
+            <hr class="sidebar-divider">
+            <!-- query dari menu -->
+            <?php
+            $role_id = $this->session->userdata('role_id');
+            $queryMenu = "SELECT `user_menu`.`id`, `menu`
+                            FROM `user_menu` JOIN `user_access_menu` 
+                            ON `user_menu`.`id` = `user_access_menu`.`menu_id`
+                            WHERE `user_access_menu`.`role_id` = $role_id
+                            ORDER BY `user_access_menu`.`menu_id` ASC
+                            ";
+
+            $menu = $this->db->query($queryMenu)->result_array();
+            ?>
+
+            <!-- Looping menu -->
+            <?php foreach ($menu as $m) : ?>
+                <div class="sidebar-heading">
+                    <?= $m['menu']; ?>
+                </div>
+                <!-- sub menu -->
+                <?php
+                $menuId = $m['id'];
+                $querySubMenu = "SELECT *
+                FROM `user_sub_menu` 
+                WHERE `menu_id` = $menuId AND `is_active` = 1";
+
+                $subMenu = $this->db->query($querySubMenu)->result_array();
+                ?>
+                <!-- sub menu -->
+                <?php foreach ($subMenu as $sm) : ?>
+                    <li class="nav-item">
+                        <a class="nav-link" href="<?= base_url($sm['url']); ?>">
+                            <i class="<?= $sm['icon']; ?>"></i>
+                            <span><?= $sm['title']; ?></span></a>
+                    </li>
+
+                <?php endforeach; ?>
+                <hr class="sidebar-divider">
+
+            <?php endforeach; ?>
+            <li class="nav-item">
+                <a class="nav-link" href="<?= base_url('auth/logout'); ?>">
+                    <i class="fas fa-fw fa-sign-out-alt"></i>
+                    <span>Logout</span></a>
+            </li>
+
+            <!-- Divider -->
+            <hr class="sidebar-divider d-none d-md-block">
+
+            <!-- Sidebar Toggler (Sidebar) -->
+            <div class="text-center d-none d-md-inline">
+                <button class="rounded-circle border-0" id="sidebarToggle"></button>
+            </div>
+
+        </ul>
+        <!-- End of Sidebar -->
